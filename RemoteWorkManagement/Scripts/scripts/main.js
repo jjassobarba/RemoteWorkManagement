d0b3ddbae985992b0e4cb3c963c0329e9a76547f
Angel Jasso <jjassobarba@gmail.com>
2014-06-20 00:06:16 -0500

    Added layout to UserPartials/_CreateUser.cshtml

--- RemoteWorkManagement/Scripts/scripts/main.js
+++ RemoteWorkManagement/Scripts/scripts/main.js
@@ -1,3 +1,37 @@
 ﻿$(document).ready(function () {
     $("#tabs").tabs();
+    
+    $("#uploadImageArea").ace_file_input({
+        style: 'well',
+        btn_choose: 'Drop image here or click to choose',
+        btn_change: null,
+        no_icon: 'icon-picture',
+        droppable: true,
+        thumbnail: 'small',
+        preview_error: function (filename, error_code) {
+            //name of the file that failed
+            //error_code values
+            //1 = 'FILE_LOAD_FAILED',
+            //2 = 'IMAGE_LOAD_FAILED',
+            //3 = 'THUMBNAIL_FAILED'
+            //alert(error_code);
+        }
+
+    }).on('change', function () {
+        //console.log($(this).data('ace_input_files'));
+        //console.log($(this).data('ace_input_method'));
+    });
+    
+    $('#uploadImageButton').ace_file_input({
+        no_file: 'No File ...',
+        btn_choose: 'Choose',
+        btn_change: 'Change',
+        droppable: false,
+        onchange: null,
+        thumbnail: true,
+        whitelist:'gif|png|jpg|jpeg'
+        //blacklist:'exe|php'
+        //onchange:''
+        //
+    });
 });
