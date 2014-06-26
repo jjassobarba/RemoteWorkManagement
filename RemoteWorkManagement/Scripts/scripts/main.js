9a8e619e891f116a0909bff1260187a0695caf83
Angel Jasso <jjassobarba@gmail.com>
2014-06-26 14:27:10 -0500

    Added Notifications.cshtml

--- RemoteWorkManagement/Scripts/scripts/main.js
+++ RemoteWorkManagement/Scripts/scripts/main.js
@@ -48,10 +48,11 @@
     });
 
     $("#selectColaborator").chosen();
+    $("#editColaborator").chosen();
     
     $.widget("ui.dialog", $.extend({}, $.ui.dialog.prototype, {
         _title: function (title) {
-            var $title = this.options.title || '&nbsp;'
+            var $title = this.options.title || '&nbsp;';
             if (("title_html" in this.options) && this.options.title_html == true)
                 title.html($title);
             else title.text($title);
@@ -61,10 +62,11 @@
     $("#addEmailButton").on('click', function (e) {
         e.preventDefault();
 
-        var dialog = $("#email-dialog").removeClass('hide').dialog({
+        $("#email-dialog").removeClass('hide').dialog({
             modal: true,
-            title: "<div class='widget-header widget-header-small'><h4 class='smaller'><i class='icon-ok'></i> jQuery UI Dialog</h4></div>",
+            title: "<div class='widget-header widget-header-small'><h4 class='small'><i class='icon-ok'></i>Email Notifications</h4></div>",
             title_html: true,
+            width: 350,
             buttons: [
                 {
                     text: "Cancel",
