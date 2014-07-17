3b0e00c6f24c468cb61b6f4c29592316bc6ef353
Angel Jasso <jjassobarba@gmail.com>
2014-07-17 01:13:36 -0500

    Added Directives/popup.js to bind elemnts

--- RemoteWorkManagement/Scripts/app/Controllers/NotificationsCtrl.js
+++ RemoteWorkManagement/Scripts/app/Controllers/NotificationsCtrl.js
@@ -3,25 +3,28 @@
         //---------------------------Variables Declaration---------------------
         $scope.users = [];
         $scope.showInfo = false;
-        
+
         //---------------------------------------------------------------------
 
         //---------------------------Public Functions--------------------------
         //GET
         $scope.getUsers = function () {
             userService.getAllUsers().then(function (response) {
-                response.users.map(function(d) {
+                response.users.map(function (d) {
                     var user = {};
                     user.id = d.Id;
                     user.name = d.Name;
                     $scope.users.push(user);
                 });
+                $scope.users = $scope.users.sortBy(function (d) {
+                    return d.name;
+                });
             });
         };
         $scope.getUsers();
 
-        $scope.getUser = function() {
-            userService.getUser($scope.selectedUser).then(function(response) {
+        $scope.getUser = function () {
+            userService.getUser($scope.selectedUser).then(function (response) {
                 $scope.fullName = response.userInfo.FirstName + " " + response.userInfo.LastName;
                 $scope.email = response.userInfo.IdMembership.Email;
                 $scope.position = response.userInfo.Position;
---
+++ RemoteWorkManagement/Scripts/app/Directives/popup.js
@@ -0,0 +1,26 @@
+﻿(function () {
+    angular.module('RemoteManagement').directive('popup', function () {
+        return {
+            restrict: 'A',
+            link: function (scope, element, attrs) {
+                element.bind('mouseenter', function () {
+                    var $this = $(this);
+                    var $parent = $this.closest('.tab-pane');
+
+                    var off1 = $parent.offset();
+                    var w1 = $parent.width();
+
+                    var off2 = $this.offset();
+                    var w2 = $this.width();
+
+                    var place = 'left';
+                    if (parseInt(off2.left) < parseInt(off1.left) + parseInt(w1 / 2)) place = 'right';
+
+                    $this.find('.popover').removeClass('right left').addClass(place);
+                }).on('click', function () {
+                    return false;
+                });
+            }
+        };
+    });
+})();
