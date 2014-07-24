67bdf1db7f6482295dbc889df18781a3aff60a13
Angel Jasso <jajasso@sciodev.com>
2014-07-24 17:43:24 -0500

    Added Entities/CheckInOut.cs and Entities/Messages.cs

---
+++ Scio.RemoteManagementModels/Entities/CheckInOut.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Scio.RemoteManagementModels.Entities
+{
+    public class CheckInOut
+    {
+        public virtual Guid IdCheck { get; set; }
+        public virtual UserInfo IdUserInfo { get; set; }
+        public virtual DateTime CheckInDate { get; set; }
+        public virtual DateTime CheckOutDate { get; set; }
+
+    }
+}
---
+++ Scio.RemoteManagementModels/Entities/Messages.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Scio.RemoteManagementModels.Entities
+{
+    public class Messages
+    {
+        public virtual Guid IdMessage { get; set; }
+        public virtual UserInfo IdUserInfo { get; set; }
+        public virtual Guid IdTo { get; set; }
+        public virtual string Message { get; set; }
+    }
+}
