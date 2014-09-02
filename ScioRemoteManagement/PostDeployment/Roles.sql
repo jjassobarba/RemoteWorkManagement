a94a19e7c55f31d284c8366fe2df8b8307cc6389
David Romo <jdavidromo@live.com.mx>
2014-09-02 13:33:16 -0500

    Added - default user into script

--- ScioRemoteManagement/PostDeployment/Roles.sql
+++ ScioRemoteManagement/PostDeployment/Roles.sql
@@ -15,3 +15,15 @@ INSERT INTO Roles SELECT 'Admin','RemoteWorkManagement' WHERE NOT EXISTS (SELECT
 INSERT INTO Roles SELECT 'TeamLeader','RemoteWorkManagement' WHERE NOT EXISTS (SELECT 2 FROM Roles WHERE Id = 2)
 INSERT INTO Roles SELECT 'TeamMember','RemoteWorkManagement' WHERE NOT EXISTS (SELECT 3 FROM Roles WHERE Id = 3)
 INSERT INTO Roles SELECT 'Sensei','RemoteWorkManagement' WHERE NOT EXISTS (SELECT 4 FROM Roles WHERE Id = 4)
+
+PRINT 'inserting User'
+
+insert into users(UserName,ApplicationName,Email,PassWord,Comment,PasswordQuestion,PasswordAnswer,IsApproved,
+LastActivityDate,LastLoginDate,LastPasswordChangedDate,CreationDate,IsOnLine,IsLockedOut,LastLockedOutDate,FailedPasswordAttemptCount,
+FailedPasswordAttemptWindowStart,FailedPasswordAnswerAttemptCount,FailedPasswordAnswerAttemptWindowStart)  
+values('jdromo@sciodev.com','RemoteWorkManagement','jdromo@sciodev.com',HashBytes('MD5', 'admin'),'','',HashBytes('MD5', 'rpta'),1,
+GETDATE(),GETDATE(),GETDATE(),GETDATE(),0,0,GETDATE(),0,
+GETDATE(),0,GETDATE());
+
+insert into UserInfo(IdUserInfo,IdMembership,FirstName,LastName,IsTemporalPassword)
+values (NEWID(),1,'AdminName','AdminLstName',0);
