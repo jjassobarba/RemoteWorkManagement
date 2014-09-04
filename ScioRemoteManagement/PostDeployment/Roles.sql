d461c2e129109e0bfdef90ee7dfea175a672b481
David Romo <jdavidromo@live.com.mx>
2014-09-04 15:56:04 -0500

    Modified - PostDeployment script

--- ScioRemoteManagement/PostDeployment/Roles.sql
+++ ScioRemoteManagement/PostDeployment/Roles.sql
@@ -17,7 +17,6 @@ INSERT INTO Roles SELECT 'TeamMember','RemoteWorkManagement' WHERE NOT EXISTS (S
 INSERT INTO Roles SELECT 'Sensei','RemoteWorkManagement' WHERE NOT EXISTS (SELECT 4 FROM Roles WHERE Id = 4)
 
 PRINT 'Inserting User'
-
 INSERT INTO Users SELECT 'admin@rewoma.com','RemoteWorkManagement','admin@rewoma.com','','k1Oo4AWmZ7G9WfWMSTC9Hw9IuSA=','','k1Oo4AWmZ7G9WfWMSTC9Hw9IuSA=',1,
 GETDATE(),GETDATE(),GETDATE(),GETDATE(),0,0,GETDATE(),0,GETDATE(),0,GETDATE() WHERE NOT EXISTS (SELECT 1 FROM Users WHERE Id=1);
 
