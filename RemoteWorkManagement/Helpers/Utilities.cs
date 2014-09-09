1d75a9cab0aa517241d511fa2e166fb3d151f8a0
Angel Jasso <jjassobarba@gmail.com>
2014-09-09 17:18:41 -0500

    Added email templates

--- RemoteWorkManagement/Helpers/Utilities.cs
+++ RemoteWorkManagement/Helpers/Utilities.cs
@@ -6,8 +6,13 @@ namespace RemoteWorkManagement.Helpers
 {
     public class Utilities
     {
-        private const string NewUserTemplate = "<html><head><title> Welcome to Remote Work Administration System! </title></head><body style='font-family: sans-serif, helvetica, arial;'><img src='http://s9.postimg.org/6suk6q4vj/Banner_Rewoma.png' alt='Remote Logo'><h3 style='color: #5090C1;'> Remote Work Administration System! </h3><p>This e-mail confirms that you are registered on REWOMA system. <h5>YOUR ACCOUNT INFORMATION:</h5><ul><li style='list-style-type: none;'><b>Password:</b> @password (you can change it the first time you login to the system)</li></ul></p><p>Go to <a href='http://remotework.scio.local/Account/Login'>REWOMA</a> to start reporting your remote work activity.</p><p>If you need assistance for login to the system, please contact HR Department by sending an e-mail to <a href='mailto:hr@sciodev.com'>hr@sciodev.com</a></p><p>Regards.</p><p>HR Team</p><p style='height: 30px; background-color: #e2e2e2;'>&copy Scio Consulting</p></body></html>";
-        private const string RecoverPasswordTemplate = "<html><head><title> Recover Password </title></head><body style='font-family: sans-serif, helvetica, arial;'><img src='http://s9.postimg.org/6suk6q4vj/Banner_Rewoma.png' alt='Remote Logo'><h3 style='color: #5090C1;'> Recover Password </h3><p>These are your temporal new credentials:<h5>YOUR ACCOUNT INFORMATION:</h5><ul><li style='list-style-type: none;'><b>Password:</b> @password</li></ul></p><p>Go to <a href='http://remotework.scio.local/Account/Login'>REWOMA</a> to start reporting your remote work activity.</p><p>If you need assistance for login to the system, please contact HR Department by sending an e-mail to <a href='mailto:hr@sciodev.com'>hr@sciodev.com</a></p><p>Regards.</p><p>HR Team</p><p style='height: 30px; background-color: #e2e2e2;'>&copy Scio Consulting</p></body></html>";
+        private const string NewUserTemplate = "<html><head><title> Welcome to Remote Work Administration System! </title></head><body style='font-family: sans-serif, helvetica, arial;'><img src='http://s9.postimg.org/6suk6q4vj/Banner_Rewoma.png' alt='Remote Logo'><h3 style='color: #5090C1;'> Remote Work Administration System! </h3><p>This e-mail confirms that you are registered on REWOMA system. <h5>YOUR ACCOUNT INFORMATION:</h5><ul><li style='list-style-type: none;'><b>Password:</b> @fieldValue (you can change it the first time you login to the system)</li></ul></p><p>Go to <a href='http://remotework.scio.local/Account/Login'>REWOMA</a> to start reporting your remote work activity.</p><p>If you need assistance for login to the system, please contact HR Department by sending an e-mail to <a href='mailto:hr@sciodev.com'>hr@sciodev.com</a></p><p>Regards.</p><p>HR Team</p><p style='height: 30px; background-color: #e2e2e2;'>&copy Scio Consulting</p></body></html>";
+        private const string RecoverPasswordTemplate = "<html><head><title> Recover Password </title></head><body style='font-family: sans-serif, helvetica, arial;'><img src='http://s9.postimg.org/6suk6q4vj/Banner_Rewoma.png' alt='Remote Logo'><h3 style='color: #5090C1;'> Recover Password </h3><p>These are your temporal new credentials:<h5>YOUR ACCOUNT INFORMATION:</h5><ul><li style='list-style-type: none;'><b>Password:</b> @fieldValue</li></ul></p><p>Go to <a href='http://remotework.scio.local/Account/Login'>REWOMA</a> to start reporting your remote work activity.</p><p>If you need assistance for login to the system, please contact HR Department by sending an e-mail to <a href='mailto:hr@sciodev.com'>hr@sciodev.com</a></p><p>Regards.</p><p>HR Team</p><p style='height: 30px; background-color: #e2e2e2;'>&copy Scio Consulting</p></body></html>";
+        private const string ChangeRemoteDayTemplate = "<html><head><title> Change Remote Day </title></head><body style='font-family: sans-serif, helvetica, arial;'><img src='http://s9.postimg.org/6suk6q4vj/Banner_Rewoma.png' alt='Remote Logo'><h3 style='color: #5090C1;'> Change Remote Day </h3><p>The user <b>@username</b> has a new request to change his day to work in remote </p><p>Log in <a href='http://remotework.scio.local/Account/Login'>REWOMA</a> to follow the status.</p><p>HR Team</p><p style='height: 30px; background-color: #e2e2e2;'>&copy Scio Consulting</p></body></html>";
+        private const string RequestExceptionTemplate = "<html><head><title> Request Exception </title></head><body style='font-family: sans-serif, helvetica, arial;'><img src='http://s9.postimg.org/6suk6q4vj/Banner_Rewoma.png' alt='Remote Logo'><h3 style='color: #5090C1;'> Request Exception </h3><p>The user <b>@username</b> wants an exception to work in remote. </p><p>Log in <a href='http://remotework.scio.local/Account/Login'>REWOMA</a> to follow the status.</p><p>HR Team</p><p style='height: 30px; background-color: #e2e2e2;'>&copy Scio Consulting</p></body></html>";
+        private const string CheckInTemplate = "<html><head><title> Check In Template </title></head><body style='font-family: sans-serif, helvetica, arial;'><img src='http://s9.postimg.org/6suk6q4vj/Banner_Rewoma.png' alt='Remote Logo'><h3 style='color: #5090C1;'> Check In </h3><p>Hello Mates, I am able to star working, feel free to contact me.</p><p><b>@fullname</b></p><p style='height: 30px; background-color: #e2e2e2;'>&copy Scio Consulting</p></body></html>";
+        private const string CheckOutTemplate = "<html><head><title> Change Remote Dat </title></head><body style='font-family: sans-serif, helvetica, arial;'><img src='http://s9.postimg.org/6suk6q4vj/Banner_Rewoma.png' alt='Remote Logo'><h3 style='color: #5090C1;'> Check Out </h3><p>My shift for today is done. I'll be offline.</p><p><b>@fullname</b></p><p style='height: 30px; background-color: #e2e2e2;'>&copy Scio Consulting</p></body></html>";
+
 
 
         /// <summary>
@@ -27,7 +32,7 @@ namespace RemoteWorkManagement.Helpers
         /// emails sender.
         /// </summary>
         /// <param name="mailto">The mailto.</param>
-        /// <param name="password">The password.</param>
+        /// <param name="password">The fieldValue.</param>
         /// <param name="type">The type.</param>
         /// <returns></returns>
         public static bool MailSender(string mailto, string password, EmailType type)
@@ -60,19 +65,30 @@ namespace RemoteWorkManagement.Helpers
         /// Checks the email template.
         /// </summary>
         /// <param name="emailType">Type of the email.</param>
-        /// <param name="password">The password.</param>
-        private static string CheckEmailTemplate(EmailType emailType, string password)
+        /// <param name="fieldValue">The fieldValue.</param>
+        private static string CheckEmailTemplate(EmailType emailType, string fieldValue)
         {
             string body = null;
             switch (emailType)
             {
                 case EmailType.Welcome:
-                    body = NewUserTemplate.Replace("@password", password);
+                    body = NewUserTemplate.Replace("@fieldValue", fieldValue);
                     break;
                 case EmailType.ForgotPassword:
-                    body = RecoverPasswordTemplate.Replace("@password", password);
+                    body = RecoverPasswordTemplate.Replace("@fieldValue", fieldValue);
                     break;
-
+                case EmailType.ChangeRemoteDay:
+                    body = ChangeRemoteDayTemplate.Replace("@username", fieldValue);
+                    break;
+                case EmailType.RequestException:
+                    body = RequestExceptionTemplate.Replace("@username", fieldValue);
+                    break;
+                case EmailType.CheckIn:
+                    body = CheckInTemplate.Replace("@fullname", fieldValue);
+                    break;
+                case EmailType.CheckOut:
+                    body = CheckOutTemplate.Replace("@fullname", fieldValue);
+                    break;
             }
             return body;
         }
